fd90372f4daf2d34d2a66dcbfb90ce08def9c2a5
yasin2514 <yasinkhanrabbi1999@gmail.com>
2022-07-31 13:22:57 +0600

    second push

--- js/calculator.js
+++ js/calculator.js
@@ -2,8 +2,6 @@
 function pressNumberButton(number, numberId) {
     const numberInputId = document.getElementById(numberId);
     numberInputId.addEventListener('click', function () {
-        // console.log('clicked btn');
-
         const displayNumber = document.getElementById('display-number');
         displayNumber.innerText = displayNumber.innerText + number;
         const displayNumberValue = displayNumber.innerText;
@@ -30,7 +28,7 @@ function pressOperatorButton(operator, operatorId, isCalculation) {
 
 
 }
-function pressEqualButton(btnId, expression) {
+function pressEqualButton(btnId) {
     const resultBtn = document.getElementById(btnId)
     resultBtn.addEventListener('click', function () {
         // for number-1
@@ -45,15 +43,44 @@ function pressEqualButton(btnId, expression) {
         const getNumber2Value = parseFloat(getNumber2Text);
         const storeNumber2 = document.getElementById('store-number2');
         storeNumber2.innerText = getNumber2Value;
-
         getNumber2.innerText = '';
-        // console.log('number2:', storeNumber2Value);
-        if (expression == 'sum') {
-            const sumTotal = getNumber1Value + getNumber2Value;
-            // console.log('total', sumTotal);
-            document.getElementById('display-result').innerText = sumTotal;
+
+        const expression = document.getElementById('display-operator');
+        const expressionValue = expression.innerText;
+
+        if (expressionValue == '+') {
+            const totalResult = getNumber1Value + getNumber2Value;
+            document.getElementById('display-result').innerText = totalResult;
 
         }
+        else if (expressionValue == '-') {
+            const totalResult = getNumber1Value - getNumber2Value;
+            document.getElementById('display-result').innerText = totalResult;
+
+        }
+        else if (expressionValue == '*') {
+            const totalResult = getNumber1Value * getNumber2Value;
+            document.getElementById('display-result').innerText = totalResult;
+
+        }
+        else if (expressionValue == '/') {
+            const totalResult = getNumber1Value / getNumber2Value;
+            document.getElementById('display-result').innerText = totalResult;
+
+        }
+        else {
+            const totalResult = getNumber1Value % getNumber2Value;
+            document.getElementById('display-result').innerText = totalResult;
+
+        }
+
+    })
+}
+function pressCbtn(btnId) {
+    document.getElementById(btnId).addEventListener('click', function () {
+
+        const clearDisplayNumber = document.getElementById('display-number');
+        clearDisplayNumber.innerText = '';
 
     })
 }
@@ -67,20 +94,20 @@ function pressACbtn(btnId) {
         clearOperator.innerText = '';
         const clearResult = document.getElementById('display-result');
         clearResult.innerText = '';
+        const clearDisplayNumber = document.getElementById('display-number');
+        clearDisplayNumber.innerText = '';
 
     })
+
 }
-//equal btn
-pressEqualButton('result', 'sum');
-//  AC btn
-pressACbtn('ac');
+
 // all number btn
 pressNumberButton(0, 'zero');
 pressNumberButton(1, 'one');
 pressNumberButton(2, 'two');
-pressNumberButton(2, 'three');
-pressNumberButton(3, 'four');
-pressNumberButton(4, 'five');
+pressNumberButton(3, 'three');
+pressNumberButton(4, 'four');
+pressNumberButton(5, 'five');
 pressNumberButton(6, 'six');
 pressNumberButton(7, 'seven');
 pressNumberButton(8, 'eight');
@@ -92,5 +119,11 @@ pressOperatorButton('-', 'sub', 'sub');
 pressOperatorButton('*', 'mul', 'mul');
 pressOperatorButton('/', 'div', 'div');
 pressOperatorButton('%', 'mod', 'mod');
+//equal btn
+pressEqualButton('result');
+//  AC btn
+pressCbtn('clear');
+//  C btn
+pressACbtn('ac');
 
 
